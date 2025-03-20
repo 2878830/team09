d8acbb8603f6ce9ab92a079de7fc6453652febda
2878830 <2978830C@student.gla.ac.uk>
2025-03-20 12:37:40 +0000

    Update mixin_utils.py

--- apps/utils/mixin_utils.py
+++ apps/utils/mixin_utils.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 
-# 登录验证
+
 class LoginRequiredMixin(object):
     @method_decorator(login_required(login_url='/login/'))
     def dispatch(self, request, *args, **kwargs):
