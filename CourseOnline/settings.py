97d71f98cb401df53432a1e219ecf689177581a6
2878830 <2978830C@student.gla.ac.uk>
2025-03-20 15:54:06 +0000

    Update settings.py

--- CourseOnline/settings.py
+++ CourseOnline/settings.py
@@ -128,12 +128,12 @@ if os.getenv('DJANGO_PRODUCTION', 'False') == 'True':
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
-EMAIL_HOST = "smtp.qq.com"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587   
-EMAIL_HOST_USER = "1301510900@qq.com"
-EMAIL_HOST_PASSWORD = "erurhmhvjlusjijf"
+EMAIL_HOST_USER = "o6889921@gmail.com"
+EMAIL_HOST_PASSWORD = "trdh jxhk rgij fddo"
 EMAIL_USE_TLS = True
-EMAIL_FROM = "1301510900@qq.com"
+EMAIL_FROM = "o6889921@gmail.com"
 
 
 MEDIA_URL = '/media/'
