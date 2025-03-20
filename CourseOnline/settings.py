042a68d0bc3aa099c4e205a4115aa66f9adbdb1d
2878830 <2978830C@student.gla.ac.uk>
2025-03-20 12:39:17 +0000

    Update settings.py

--- CourseOnline/settings.py
+++ CourseOnline/settings.py
@@ -1,32 +1,30 @@
 import os
 import sys
 
-# 项目路径
+
 from apps import operation, courses, users
 from extra_apps import  DjangoUeditor
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# 将这两个目录添加到环境变量
+
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
-# 项目秘钥
+
 SECRET_KEY = '+1nruftzf9)mi3mb!0_wpsmw=pne(717j(xszq_ghmap2024zq'
 
-# 调试模式
+
 DEBUG = True
 
-# 允许的主机
 ALLOWED_HOSTS = ['*']
 
-# Application definition 注册我们的app
-# 设置邮箱和用户名均可登录
+# Application definition 
 AUTHENTICATION_BACKENDS = (
     'users.views.CustomBackend',
 )
 
-# 注册app
+# app
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -34,21 +32,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',  # 用户管理模块
-    'courses',  # 课程管理模块
-    'operation',  #
-    'organization',  # 机构管理模块
-    'xadmin',  # 后台管理系统
-    'crispy_forms',  # 表单
-    'captcha',  # 验证码
-    'pure_pagination',  # 分页
-    'DjangoUeditor',  # 富文本编辑器
+    'users', 
+    'courses',  
+    'operation', 
+    'organization', 
+    'xadmin', 
+    'crispy_forms',  
+    'captcha', 
+    'pure_pagination', 
+    'DjangoUeditor', 
 ]
 
-# 此处重载是为了使我们的UserProfile生效
+
 AUTH_USER_MODEL = "users.UserProfile"
 
-# 中间件
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -59,10 +57,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# 根路由
+
 ROOT_URLCONF = 'CourseOnline.urls'
 
-# 前端模板文件
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -84,19 +82,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'CourseOnline.wsgi.application'
 
-# 数据库配置
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'team09three$db_course_online',  # 数据库名称
-        'USER': 'team09three',  # 账号
-        'PASSWORD': '123456it',  # 密码
+        'NAME': 'team09three$db_course_online',  
+        'USER': 'team09three', 
+        'PASSWORD': '123456it',  
         'HOST': 'team09three.mysql.pythonanywhere-services.com'
 
     }
 }
 
-# 密码验证
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -118,7 +116,7 @@ TIME_ZONE = 'Europe/London'
 USE_I18N = True
 USE_L10N = True
 
-# 数据库存储使用时间，True时间会被存为UTC的时间
+
 USE_TZ = True
 
 #  (CSS, JavaScript, Images)
@@ -129,7 +127,7 @@ STATICFILES_DIRS = [
 if os.getenv('DJANGO_PRODUCTION', 'False') == 'True':
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# 发送邮件的setting设置
+
 EMAIL_HOST = "smtp.qq.com"
 EMAIL_PORT = 587   
 EMAIL_HOST_USER = "1301510900@qq.com"
@@ -137,6 +135,6 @@ EMAIL_HOST_PASSWORD = "erurhmhvjlusjijf"
 EMAIL_USE_TLS = True
 EMAIL_FROM = "1301510900@qq.com"
 
-# 设置我们上传文件的路径
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
